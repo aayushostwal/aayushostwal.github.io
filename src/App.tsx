39200dc24000f33e0a7368f23159988c74c2710c
ostwal99 <aayush.ostwal@qure.ai>
2024-12-05 23:32:04 +0530

    Made basic wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { Box, MantineProvider } from "@mantine/core";
+import CursorShadowProvider from "./components/CursorShadow";
+import AppLayout from "./components/Layout";
 
-import { Button } from '@mantine/core';
-
-const App = () => (
-  <div>
-    <h1>Hello, GitHub Pages!</h1>
-    <Button>Hello Mantine</Button>
-  </div>
-);
-
-export default App;
+export default function App() {
+  return (
+    <MantineProvider>
+      <CursorShadowProvider>
+        <Box
+          style={{
+            width: "100vw",
+            height: "100vh",
+          }}
+        >
+          <AppLayout />
+        </Box>
+      </CursorShadowProvider>
+    </MantineProvider>
+  );
+}
