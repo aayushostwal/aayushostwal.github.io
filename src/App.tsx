45e6d20703f6578a5573598aabdea74503b80f06
ostwal99 <aayush.ostwal@qure.ai>
2025-01-27 09:42:16 +0530

    Added Vercel Insights

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ export default function App() {
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </BrowserRouter>
+		  <SpeedInsights />
         </Box>
       </CursorShadowProvider>
     </MantineProvider>
